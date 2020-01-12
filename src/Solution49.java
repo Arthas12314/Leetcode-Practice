c859d905e22b8dd1712c42c178f05807cfe8eb17
Arthas12314 <csczjy@163.com>
2020-01-13 01:02:30 +0800

    2020.1.12

---
+++ src/Solution49.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/*
+* 在美版leetcode上看到大神的思路，用质数表示26个字母，把字符串的各个字母相乘，
+* 这样可保证字母异位词的乘积必定是相等的。其余步骤就是用map存储，和别人的一致了
+*
+* 待实现
+* */
+public class Solution49 {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res=new ArrayList<>();
+        HashMap<String,ArrayList<String>> map=new HashMap<>();
+        for (String str : strs) {
+            char[] s = str.toCharArray();
+            Arrays.sort(s);
+            String key = String.valueOf(s);
+            if (!map.containsKey(key))
+                map.put(key, new ArrayList<>());
+            map.get(key).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    public static void main(String[] args) {
+        String[] strs={"eat","tea","tan","ate","nat","bat"};
+        Solution49 t=new Solution49();
+        System.out.println(t.groupAnagrams(strs));
+    }
+}
