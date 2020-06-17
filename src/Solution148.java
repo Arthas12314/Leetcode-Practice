220f5e858f28ca978740c5b79a5d52bb9b6f8e7a
RyougiShiki <csczjy@163.com>
2020-06-17 23:04:08 +0800

    2020.6.17 Update

---
+++ src/Solution148.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+
+/**
+ * @author Arthas
+ */
+public class Solution148 {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode slow = head, fast = head.next;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        fast = slow.next;
+        slow.next = null;
+        head = sortList(head);
+        fast = sortList(fast);
+        return mergeList(head, fast);
+    }
+
+    private ListNode mergeList(ListNode node1, ListNode node2) {
+        ListNode pre = new ListNode(-1);
+        ListNode cur = pre;
+        while (node1 != null && node2 != null) {
+            if (node1.val > node2.val) {
+                cur.next = node2;
+                node2=node2.next;
+            } else {
+                cur.next = node1;
+                node1=node1.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = node1 == null ? node2 : node1;
+        return pre.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(-1);
+        ListNode cur = listNode;
+        Random random = new Random();
+        for (int i = 0; i < 10; i++) {
+            cur.next = new ListNode(random.nextInt()%10);
+            cur=cur.next;
+        }
+        Solution148 t = new Solution148();
+        System.out.println(t.sortList(listNode));
+    }
+}
